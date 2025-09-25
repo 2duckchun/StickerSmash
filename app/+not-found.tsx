c97d41795d2076d0664f347dea402ec80cd22ced
2DC <kkts9308@gmail.com>
2025-09-25 23:28:21 +0900

    :art: add not-found page

---
+++ app/+not-found.tsx
@@ -0,0 +1,33 @@
+import { Link, Stack } from "expo-router";
+import { StyleSheet, View } from "react-native";
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          title: "Oops! Not Found",
+        }}
+      />
+      <View style={style.container}>
+        <Link href="/" style={style.button}>
+          Go Back to Home Screen!
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+});
